519cee685fa82f5139d882b31c6eb1042e2c1286
Peikai Zheng <bnuzpk@gmail.com>
2018-11-20 18:56:47 +0800

    refactor

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ TESTID = 'mi584oior0veus2f79lolaht54@group.calendar.google.com'
 ALGOID = 'o3alvrvraeam3vd43kqent15p4@group.calendar.google.com'
 CURRENTID = ALGOID
 
-
 def CodeforcesTime2ISO(str_time, str_length):
   datetime_start = datetime.strptime(str_time, '%b/%d/%Y %H:%M')
   datetime_length = datetime.strptime(str_length, '%H:%M')
@@ -32,94 +31,87 @@ def AtcoderTime2ISO(str_time, str_length):
     datetime_end = datetime_start + timedelta_length
     return datetime_start.isoformat()+'+09:00', datetime_end.isoformat() + '+09:00'
 
-
-def AddCodeforcesEvents(service, calendar_event_summary_list):
-
-  result = requests.get("http://codeforces.com/contests")
-  soup = BeautifulSoup(result.text)
+def ParseCodeforcesEvents(text):
+  soup = BeautifulSoup(text, 'lxml')
   first_table = soup.find('table')
   contests = first_table.find_all('tr')[1:]
 
-  event = {
-    'summary': 'None',
-    'description': 'Codeforces',
-    'start': {
-        'dateTime': 'None',
-    },
-    'end': {
-        'dateTime': 'None',
-    },
-      'reminders': {
-      'useDefault': False,
-      'overrides': [
-        {'method': 'popup', 'minutes': 10},
-      ],
-    },
-  }
-
+  contest_list = []
   for contest in contests:
     name, writers, start, length = [element.get_text().strip() for element in contest.find_all('td')[:4]]
-    #print(name, writers, start, length)
-    str_start, str_end = CodeforcesTime2ISO(start, length)
-    #print(str_start, str_end)
-    if name in calendar_event_summary_list:
-      print(name, 'has been added')
-    else:
-      event['summary'] = name
-      event['start']['dateTime'] = str_start
-      event['end']['dateTime'] = str_end
-      insert_result = service.events().insert(calendarId=CURRENTID, body=event).execute()
-      print(insert_result)
+    contest_list.append((name, start, length))
+  return contest_list
 
-def AddAtcoderEvents(service, calendar_event_summary_list):
-
-  result = requests.get("http://atcoder.jp/contest")
-  soup = BeautifulSoup(result.text)
+def ParseAtcoderEvents(text):
+  soup = BeautifulSoup(text, 'lxml')
   first_table = soup.find_all('table', class_='table')[1]
   contests = first_table.find('tbody').find_all('tr')
 
-  event = {
-    'summary': 'None',
-    'description': 'AtCoder',
-    'start': {
-        'dateTime': 'None',
-    },
-    'end': {
-        'dateTime': 'None',
-    },
-      'reminders': {
-      'useDefault': False,
-      'overrides': [
-        {'method': 'popup', 'minutes': 10},
-      ],
-    },
-  }
-
+  contest_list = []
   for contest in contests[::3]:
     start, name, length, limit = [element.get_text().strip() for element in contest.find_all('td')[:]][:4]
-    str_start, str_end = AtcoderTime2ISO(start, length)
-    if name in calendar_event_summary_list:
-      print(name, 'has been added')
-    else:
-      event['summary'] = name
-      event['start']['dateTime'] = str_start
-      event['end']['dateTime'] = str_end
-      insert_result = service.events().insert(calendarId=CURRENTID, body=event).execute()
-      print(insert_result)
+    contest_list.append((name, start, length))
+  return contest_list
+
+class AddEvents(object):
+
+  def __init__(self, url, website, parse_event, time2iso, event_list):
+    self.url = url
+    self.website = website
+    self.time2iso = time2iso
+    self.parse_event = parse_event
+    self.event_set = event_list
+
+  def addEvents(self, service, calendarId):
+    event = {
+      'summary': 'None',
+      'description': self.website,
+      'start': {
+          'dateTime': 'None',
+      },
+      'end': {
+          'dateTime': 'None',
+      },
+        'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+
+    html_result = requests.get(self.url)
+    contest_list = self.parse_event(html_result.text)
+
+    for contest in contest_list:
+      name, start, length = contest
+      str_start, str_end = self.time2iso(start, length)
+      if name in self.event_set:
+        print(name, 'has been added')
+      else:
+        event['summary'] = name
+        event['start']['dateTime'] = str_start
+        event['end']['dateTime'] = str_end
+        insert_result = service.events.insert(calendarId=calendarId, body=event).execute()
+        print(insert_result)
 
 
-def FetchAllEvents(service):
-  # Call the Calendar API
+def FetchAllEvents(service, calendarId):
+  """Fetching all events of the calenderId
+
+  :param service:
+  :return:
+  """
   now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
   page_token = None
   event_list = []
   while True:
-    events = service.events().list(calendarId=CURRENTID, pageToken=page_token, timeMin=now,
+    events = service.events().list(calendarId=calendarId, pageToken=page_token, timeMin=now,
                                    singleEvents=True, orderBy='startTime').execute()
     event_list.extend(events.get('items', []))
     page_token = events.get('nextPageToken')
     if not page_token:
-        break
+      break
   return event_list
 
 def main():
@@ -136,12 +128,14 @@ def main():
   # Call the Calendar API
   now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
 
-  calendar_event_list = FetchAllEvents(service)
+  calendar_event_list = FetchAllEvents(service, CURRENTID)
 
   summary_list = set([event.get('summary') for event in calendar_event_list])
 
-  AddCodeforcesEvents(service, summary_list)
-  AddAtcoderEvents(service, summary_list)
+  AddEvents(url="http://codeforces.com/contests", website="Codeforces", parse_event=ParseCodeforcesEvents,
+            time2iso=CodeforcesTime2ISO, event_list=summary_list).addEvents(service, CURRENTID)
+  AddEvents(url="http://atcoder.jp/contest", website="AtCoder", parse_event=ParseAtcoderEvents,
+            time2iso=AtcoderTime2ISO, event_list=summary_list).addEvents(service, CURRENTID)
 
 
 if __name__ == '__main__':
